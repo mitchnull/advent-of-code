e4c3adad14cefbcff3b5394a51f09c04b0bb014f
Péter Radics <mitchnull@gmail.com>
2024-12-02 16:14:07 +0100

    02a1 - std::algorithms

---
+++ src/main/cpp/02a1/main.cpp
@@ -0,0 +1,48 @@
+#include <ios>
+#include <iostream>
+#include <cctype>
+#include <cstdint>
+#include <limits>
+#include <deque>
+#include <numeric>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <algorithm>
+#include <ranges>
+#include <charconv>
+#include <sstream>
+
+using uint8 = std::uint8_t;
+using int8 = std::int8_t;
+using uint32 = std::uint32_t;
+using int32 = std::int32_t;
+using uint64 = std::uint64_t;
+using int64 = std::int64_t;
+
+using uint = uint32;
+
+using Nums = std::vector<int>;
+
+static bool
+check(const std::string& line) {
+  auto ss = std::stringstream(line);
+  auto nums = Nums(std::istream_iterator<int>(ss), std::istream_iterator<int>());
+  if (!std::is_sorted(nums.begin(), nums.end()) && !std::is_sorted(nums.begin(), nums.end(), std::greater<>())) {
+    return false;
+  }
+  std::adjacent_difference(nums.begin(), nums.end(), nums.begin());
+  return std::find_if(++nums.begin(), nums.end(), [](auto a) { return a == 0 || std::abs(a) > 3; }) == nums.end();
+}
+
+int
+main() {
+  std::string line;
+
+  int sum = 0;
+  while (std::getline(std::cin, line)) {
+    sum += check(line);
+  }
+  std::cout << sum << "\n";
+  return 0;
+}
