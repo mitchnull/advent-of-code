12ed4bdee707b589ab2cb0685a11e3f256b90074
Péter Radics <mitchnull@gmail.com>
2024-12-02 12:20:36 +0100

    01b kotlin

---
+++ src/main/kotlin/hu/desnull/aoc2024/p01b/Main.kt
@@ -0,0 +1,28 @@
+package hu.desnull.aoc2024.p01b
+
+import kotlin.math.abs
+
+private fun isWhiteSpace(c: Char) = c in " \r\n\t"
+private fun readString() = generateSequence {
+  val c = System.`in`.read()
+  if (c != -1) c.toChar() else null
+}.dropWhile { isWhiteSpace(it) }.takeWhile { !isWhiteSpace(it) }.joinToString("").ifEmpty { null }
+
+fun readLines() = generateSequence {
+  readlnOrNull()
+}
+
+fun main() {
+  val left = ArrayList<Int>()
+  val right = ArrayList<Int>()
+  readLines().forEach {
+    if (it.isEmpty()) {
+      return
+    }
+    val (l, r) = it.split("\\s+".toRegex(), 2)
+    left.add(l.toInt())
+    right.add(r.toInt())
+  }
+  val sum = left.stream().map { it * right.count { rit -> rit == it } }.reduce(Integer::sum).orElse(0)
+  println(sum)
+}
