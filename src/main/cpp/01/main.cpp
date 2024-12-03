361eb37580df097b89ec40c51bdad773b6dcebfe
Péter Radics <mitchnull@gmail.com>
2024-12-03 13:19:25 +0100

    reorg c++ versions to one file per day

---
+++ src/main/cpp/01/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <algorithm>
+
+using uint = std::uint32_t;
+
+int
+main() {
+  std::vector<int> as, bs;
+  int a, b;
+  while (std::cin >> a >> b) {
+    as.push_back(a);
+    bs.push_back(b);
+  }
+  
+  std::sort(as.begin(), as.end());
+  std::sort(bs.begin(), bs.end());
+  int res1 = 0;
+  for (uint i = 0; i < as.size(); ++i) {
+    res1 += std::abs(as[i] - bs[i]);
+  }
+  std::cout << "1: " << res1 << "\n";
+
+  // ------------------------------------------------------------------------ //
+
+  int res2 = 0;
+  for (const auto &a : as) {
+    res2 += a * std::count(bs.begin(), bs.end(), a);
+  }
+  std::cout << "2: " << res2 << "\n";
+  return 0;
+}
---
+++ src/main/cpp/02/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstdint>
+#include <numeric>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iterator>
+
+using uint = std::uint32_t;
+using Strings = std::vector<std::string>;
+using Nums = std::vector<int>;
+
+static Strings readLines() {
+  Strings lines;
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    lines.emplace_back(line);
+  }
+  return lines;
+}
+
+static bool
+check(const std::string& line) {
+  auto ss = std::stringstream(line);
+  auto nums = Nums(std::istream_iterator<int>(ss), std::istream_iterator<int>());
+  std::adjacent_difference(nums.begin(), nums.end(), nums.begin());
+  return std::find_if(nums.begin() + 1, nums.end(), [](auto a) { return a <= 0 || 3 < a; }) == nums.end() ||
+    std::find_if(nums.begin() + 1, nums.end(), [](auto a) { return a < -3 || 0 <= a; }) == nums.end();
+}
+
+template <typename T>
+static int sgn(T v) {
+  return (v < 0) ? -1 : (v > 0);
+}
+
+static int
+get(const Nums& nums, uint i, uint dampened) {
+  return nums[i + (i >= dampened)];
+}
+
+static bool
+checkDampened(const Nums& nums, uint dampened) {
+  int ls = 0;
+  int last = get(nums, 0, dampened);
+  for (uint i = 1; i < nums.size() - (dampened < nums.size()); ++i) {
+    int num = get(nums, i, dampened);
+    int diff = num - last;
+    last = num;
+    if (ls == 0) {
+      ls = sgn(diff);
+    }
+    int cs = sgn(diff);
+    if (cs == 0 || cs != ls || std::abs(diff) > 3) {
+      return false;
+    }
+  }
+  return true;
+}
+
+static bool
+checkWithDampen(const std::string& line) {
+  auto ss = std::stringstream(line);
+  auto nums = Nums(std::istream_iterator<int>(ss), std::istream_iterator<int>());
+  for (uint d = 0; d <= nums.size(); ++d) {
+    if (checkDampened(nums, d)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int
+main() {
+  Strings lines = readLines();
+
+  int res1 = std::count_if(lines.begin(), lines.end(), check);
+  std::cout << "1: " << res1 << "\n";
+
+  // ------------------------------------------------------------------------ //
+
+  int res2 = std::count_if(lines.begin(), lines.end(), checkWithDampen);
+  std::cout << "2: " << res2 << "\n";
+  return 0;
+}
