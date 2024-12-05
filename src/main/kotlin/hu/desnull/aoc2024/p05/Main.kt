c5458445f0439bb5d1b3842e177f07e0f3a35dd6
Péter Radics <mitchnull@gmail.com>
2024-12-05 09:10:14 +0100

    fiddling

--- src/main/kotlin/hu/desnull/aoc2024/p05/Main.kt
+++ src/main/kotlin/hu/desnull/aoc2024/p05/Main.kt
@@ -2,14 +2,13 @@ package hu.desnull.aoc2024.p05
 
 import java.util.Collections
 
-fun check1(rules: Set<String>, list: List<String>): Boolean {
-  return list.zipWithNext {
+fun check1(list: List<String>, rules: Set<String>): Boolean =
+  list.zipWithNext {
     a, b ->
     rules.contains("$b|$a")
   }.none { it }
-}
 
-fun order(rules: Set<String>, list: List<String>): List<String> {
+fun sort(list: List<String>, rules: Set<String>): List<String> {
   Collections.sort(list) {
     a, b ->
     if (rules.contains("$a|$b")) -1 else if (rules.contains("$b|$a")) 1 else 0
@@ -17,16 +16,17 @@ fun order(rules: Set<String>, list: List<String>): List<String> {
   return list
 }
 
+fun middle(list: List<String>): Int = list[list.size / 2].toInt()
+
 fun main() {
   val rules = generateSequence { readlnOrNull() }.takeWhile { it.isNotBlank() }.toSet()
   val lists = generateSequence { readlnOrNull() }.map { it.split(",") }.toList()
 
-  val res1 = lists.filter { check1(rules, it) }.sumOf { it[it.size / 2].toInt() }
-
+  val res1 = lists.filter { check1(it, rules) }.sumOf { middle(it) }
   println("1: $res1")
 
   // ------------------------------------------------------------------------ //
 
-  val res2 = lists.filter { !check1(rules, it) }.map { order(rules, it) }.sumOf { it[it.size / 2].toInt() }
+  val res2 = lists.filter { !check1(it, rules) }.map { sort(it, rules) }.sumOf { middle(it) }
   println("2: $res2")
 }
