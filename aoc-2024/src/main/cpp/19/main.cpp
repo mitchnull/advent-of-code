08930658cc5d2184118d354350b469306752ee71
Péter Radics <mitchnull@gmail.com>
2024-12-19 16:56:18 +0100

    19 cpp dynamic programming

--- aoc-2024/src/main/cpp/19/main.cpp
+++ aoc-2024/src/main/cpp/19/main.cpp
@@ -4,29 +4,23 @@
 #include <iterator>
 #include <sstream>
 #include <ranges>
-#include <unordered_map>
 
 using Num = uint64_t;
 using Strings = std::vector<std::string>;
-using Cache = std::unordered_map<std::string_view, Num>;
 
 static Num
-countMatches(std::string_view text, const Strings& patterns, Cache& cache) {
-  if (text.empty()) {
-    return 1;
-  }
-  auto it = cache.find(text);
-  if (it != cache.end()) {
-    return it->second;
-  }
-  Num res = 0;
-  for (std::string_view p : patterns) {
-    if (text.starts_with(p)) {
-      res += countMatches(text.substr(p.size()), patterns, cache);
+countMatches(std::string_view text, const Strings& patterns) {
+  auto ways = std::vector<Num>(text.size() + 1);
+  ways[text.size()] = 1;
+  for (int i = text.size() - 1; i >= 0; --i) {
+    std::string_view t = text.substr(i);
+    for (std::string_view p : patterns) {
+      if (t.starts_with(p)) {
+        ways[i] += ways[i + p.size()];
+      }
     }
   }
-  cache[text] = res;;
-  return res;
+  return ways.front();
 }
 
 int
@@ -41,7 +35,7 @@ main() {
   Strings targets;
   std::copy(std::istream_iterator<std::string>(std::cin), std::istream_iterator<std::string>(), std::back_inserter(targets));
 
-  auto counts = targets | std::views::transform([&patterns](std::string_view t) { Cache cache; return countMatches(t, patterns, cache); }) | std::ranges::to<std::vector<Num>>();
+  auto counts = targets | std::views::transform([&patterns](std::string_view t) { return countMatches(t, patterns); }) | std::ranges::to<std::vector<Num>>();
   auto res1 = std::count_if(counts.begin(), counts.end(), std::identity());
   auto res2 = std::reduce(counts.begin(), counts.end(), Num{});
   std::cout << "1: " << res1 << "\n";
