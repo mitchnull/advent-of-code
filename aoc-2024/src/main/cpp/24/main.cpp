319feb5513fb74c51cca1504fac4b86888ff52c3
Péter Radics <mitchnull@gmail.com>
2024-12-24 23:13:03 +0100

    24 cpp with debug prints

--- aoc-2024/src/main/cpp/24/main.cpp
+++ aoc-2024/src/main/cpp/24/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <numeric>
 #include <sstream>
 #include <algorithm>
 
@@ -18,21 +19,26 @@ using Nodes = std::vector<Node>;
 
 /* ------------------------------------------------------------------------ */
 
-static String x(int i) { return std::format("x{:02}", i); }
-static String y(int i) { return std::format("y{:02}", i); }
-static String z(int i) { return std::format("z{:02}", i); }
+static constexpr String x(int i) { return std::format("x{:02}", i); }
+static constexpr String y(int i) { return std::format("y{:02}", i); }
+static constexpr String z(int i) { return std::format("z{:02}", i); }
 
 static auto
-findNode(const Nodes& nodes, String out) {
+findNode(auto& nodes, String out) {
   return std::find_if(nodes.begin(), nodes.end(), [out](auto& n) { return n.out == out; });
 }
 
 static auto
-findNode(const Nodes& nodes, String left, String right, char op) {
-  return std::find_if(nodes.begin(), nodes.end(), [=](auto& n) {
+findNode(auto& nodes, String left, String right, char op, String name = {}, int i = 0) {
+  auto it = std::find_if(nodes.begin(), nodes.end(), [=](auto& n) {
     return n.op == op &&
       ((n.left == left && n.right == right) || 
       ((n.left == right && n.right == left))); });
+  if (!name.empty() && it == nodes.end()) {
+    std::cerr << "couldn't find node " << name << "(" << i << "): left=" << left << ", right=" << right << ", op=" << op << std::endl;
+    exit(1);
+  }
+  return it;
 }
 
 static auto
@@ -84,6 +90,63 @@ solve1(const Nodes& nodes, Outputs outs, int maxz) {
   return res;
 }
 
+static void
+swapOut(Node& a, Node& b, Strings& swaps) {
+  std::cout << "@@@ swapping " << a.out << " <=> " << b.out << std::endl;
+  swaps.push_back(a.out);
+  swaps.push_back(b.out);
+  std::swap(a.out, b.out);
+}
+
+static String
+solve2(Nodes nodes, int maxz) {
+  Strings swaps;
+  Node& z00 = *findNode(nodes, x(0), y(0), 'X'); 
+  if (z00.out != z(0)) {
+    Node& wrongNode = *findNode(nodes, z(0));
+    swapOut(z00, wrongNode, swaps);
+  }
+  auto cc = findNode(nodes, x(0), y(0), 'A', "cc", 0);
+  for (int i = 1; i < maxz; ++i) {
+    auto nn = findNode(nodes, x(i), y(i), 'X', "nn", i);
+    auto zitByIns = findNode(nodes, cc->out, nn->out, 'X');
+    auto zz = findNode(nodes, z(i));
+    if (zitByIns == nodes.end()) {
+      if (nn->out == zz->left) {
+        // cc is swapped with right
+        auto ww = findNode(nodes, zz->right);
+        std::cout << "@@@ swapping cc.out with zz.right: " << cc->out << " <=> " << ww->out << std::endl;
+        swapOut(*cc, *ww, swaps);
+      } else if (nn->out == zz->right) {
+        // cc is swapped with left
+        auto ww = findNode(nodes, zz->left);
+        std::cout << "@@@ swapping cc.out with zz.left: " << cc->out << " <=> " << ww->out << std::endl;
+        swapOut(*cc, *ww, swaps);
+      } else if (cc->out == zz->left) {
+        // nn is swapped with right
+        auto ww = findNode(nodes, zz->right);
+        std::cout << "@@@ swapping nn.out with zz.right: " << nn->out << " <=> " << ww->out << std::endl;
+        swapOut(*nn, *ww, swaps);
+      } else if (cc->out == zz->right) {
+        // nn is swapped with left
+        auto ww = findNode(nodes, zz->left);
+        std::cout << "@@@ swapping nn.out with zz.left: " << nn->out << " <=> " << ww->out << std::endl;
+        swapOut(*nn, *ww, swaps);
+      }
+    } else if (zz != zitByIns) {
+      swapOut(*zz, *zitByIns, swaps);
+    }
+    auto ss = findNode(nodes, cc->out, nn->out, 'A', "ss", i);
+    cc = findNode(nodes, x(i), y(i), 'A', "cc", i);
+    cc = findNode(nodes, cc->out, ss->out, 'O', "tt", i);
+  }
+  if (swaps.empty()) {
+    return "";
+  }
+  std::sort(swaps.begin(), swaps.end());
+  return std::accumulate(swaps.begin() + 1, swaps.end(), swaps.front(), [](auto a, auto b) { return a + ',' + b; });
+}
+
 int
 main() {
   Nodes nodes;
@@ -108,4 +171,5 @@ main() {
   }
 
   std::cout << "1: " << solve1(nodes, outs, maxz) << std::endl;
+  std::cout << solve2(nodes, maxz) << std::endl;
 }
