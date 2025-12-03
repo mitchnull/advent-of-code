a6f1a1793e05ce37f0733908869310ac113d450b
Péter Radics <mitchnull@gmail.com>
2025-12-03 07:09:38 +0100

    03 cpp

---
+++ aoc-2025/src/main/cpp/03/main.cpp
@@ -0,0 +1,64 @@
+#include <cstdint>
+#include <iostream>
+#include <print>
+#include <unordered_set>
+#include <algorithm>
+
+using Num = std::int64_t;
+using Nums = std::unordered_set<Num>;
+using std::println;
+
+static Num
+v(char c) {
+  return c - '0';
+}
+
+static Num
+jolts(char d1, char d2) {
+  return 10 * v(d1) + v(d2);
+}
+
+static Num
+jolts(std::string_view b) {
+  auto d1 = std::max_element(b.begin(), b.end());
+  auto n = d1 + 1;
+  if (n == b.end()) {
+    auto d0 = std::max_element(b.begin(), d1);
+    return jolts(*d0, *d1);
+  }
+  auto d2 = std::max_element(n, b.end());
+  return jolts(*d1, *d2);
+}
+
+static Num
+jolts(auto b, auto e, auto e1, int r) {
+  if (r == 0) {
+    return 0;
+  }
+  if (b == e1) {
+    return -1;
+  }
+  auto d1 = std::max_element(b, e1);
+  auto n = d1 + 1;
+  Num v2 = jolts(n, e, e, r - 1);
+  if (v2 >= 0) {
+    return v(*d1) * std::pow(10, r - 1) + v2;
+  }
+  return jolts(b, e, e1 - 1, r);
+}
+
+/* ------------------------------------------------------------------------ */
+
+int
+main() {
+  Num res1{}, res2{};
+  std::string s;
+  while (std::cin >> s) {
+    res1 += jolts(s);
+    res2 += jolts(s.begin(), s.end(), s.end(), 12);
+  }
+  println("1: {}", res1);
+  println("2: {}", res2);
+
+  return 0;
+}
