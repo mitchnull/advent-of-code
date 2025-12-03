390cb8787b8279b6023043bd9de2228d0893ed6e
Péter Radics <mitchnull@gmail.com>
2025-12-03 18:45:10 +0100

    simplify

--- aoc-2025/src/main/cpp/03/main.cpp
+++ aoc-2025/src/main/cpp/03/main.cpp
@@ -7,20 +7,12 @@ using Num = std::int64_t;
 using std::println;
 
 static Num
-jolts(auto b, auto e, auto e1, int r) {
+jolts(auto b, auto e, int r) {
   if (r == 0) {
     return 0;
   }
-  if (b == e1) {
-    return -1;
-  }
-  auto d1 = std::max_element(b, e1);
-  auto n = d1 + 1;
-  Num v2 = jolts(n, e, e, r - 1);
-  if (v2 < 0) {
-    return jolts(b, e, e1 - 1, r);
-  }
-  return (*d1 - '0') * std::pow(10, r - 1) + v2;
+  auto d1 = std::max_element(b, e - r + 1);
+  return (*d1 - '0') * std::pow(10, r - 1) + jolts(d1 + 1, e, r - 1);
 }
 
 /* ------------------------------------------------------------------------ */
@@ -30,8 +22,8 @@ main() {
   Num res1{}, res2{};
   std::string s;
   while (std::cin >> s) {
-    res1 += jolts(s.begin(), s.end(), s.end(), 2);
-    res2 += jolts(s.begin(), s.end(), s.end(), 12);
+    res1 += jolts(s.begin(), s.end(), 2);
+    res2 += jolts(s.begin(), s.end(), 12);
   }
   println("1: {}", res1);
   println("2: {}", res2);
