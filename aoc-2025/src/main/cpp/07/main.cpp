1371cb15fa8d9d450426cc26958dfe87769aa85a
Péter Radics <mitchnull@gmail.com>
2025-12-07 07:39:11 +0100

    unify 07

--- aoc-2025/src/main/cpp/07/main.cpp
+++ aoc-2025/src/main/cpp/07/main.cpp
@@ -4,31 +4,14 @@
 using Num = int64_t;
 using Board = Grid<>;
 
-static Num
-solve1(Board board) {
-  Num res{};
-  for (int y = 1; y < board.h(); ++y) {
-    for (int x = 0; x < board.w(); ++x) {
-      if (board[x, y - 1] == '|' || board[x, y - 1] == 'S') {
-        if (board[x, y] == '^') {
-          ++res;
-          board[x - 1, y] = board[x + 1, y] = '|';
-        } else {
-          board[x, y] = '|';
-        }
-      }
-    }
-  }
-  return res;
-}
-
-static Num
-solve2(Board board) {
-  auto ways = std::vector<Num>(board.w());
-  ++ways[std::find(board.begin(), board.end(), 'S') - board.begin()];
+static std::pair<Num, Num>
+solve(const Board& board) {
+  Num splits{};
+  auto ways = ranges::subrange(board.begin(), board.begin() + board.w()) | views::transform([](auto c) { return c == 'S'; }) | ranges::to<std::vector<Num>>();
   for (int y = 1; y < board.h(); ++y) {
     for (int x = 0; x < board.w(); ++x) {
       if (board[x, y] == '^') {
+        splits += ways[x] != 0;
         if (x > 0) {
           ways[x - 1] += ways[x];
         }
@@ -39,7 +22,7 @@ solve2(Board board) {
       }
     }
   }
-  return ranges::fold_left(ways, Num{}, std::plus<>{});
+  return {splits, ranges::fold_left(ways, Num{}, std::plus<>{})};
 }
 
 /* ------------------------------------------------------------------------ */
@@ -48,8 +31,9 @@ int
 main() {
   Board board = Board::read(std::cin, '.');
 
-  println("1: {}", solve1(board));
-  println("2: {}", solve2(board));
+  auto res = solve(board);
+  println("1: {}", res.first);
+  println("2: {}", res.second);
 
   return 0;
 }
