1bcdc4311a106fb8e7126e40d9d50f7bbdc3346d
Péter Radics <mitchnull@gmail.com>
2025-12-08 11:46:18 +0100

    08 minor cleanup

--- aoc-2025/src/main/cpp/08/main.cpp
+++ aoc-2025/src/main/cpp/08/main.cpp
@@ -5,18 +5,14 @@
 using Num = int64_t;
 
 using Pos3 = std::array<Num, 3>;
-using V = std::vector<Pos3>;
-using P = std::pair<Pos3, Pos3>;
-using PV = std::vector<P>;
-using M = std::unordered_map<Pos3, Num>;
+using Junctions = std::vector<Pos3>;
+using Groups = std::vector<int>;
 
-template <>
-struct std::hash<Pos3> {
-  std::size_t operator()(const Pos3& p) const {
-    return hashCombine(hashCombine(p[0], p[1]), p[2]);
-  }
+struct Tup {
+  int a, b;
+  Num d2;
 };
-
+using Tups = std::vector<Tup>;
 
 static Num
 d2(const Pos3& a, const Pos3& b) {
@@ -29,15 +25,15 @@ d2(const Pos3& a, const Pos3& b) {
 
 
 static Num
-solve1(const M& m) {
+solve1(const Groups& groups) {
   auto groupCounts = std::unordered_map<int, Num>{};
 
-  for (const auto& pg : m) {
-    if (pg.second != 0) {
-      ++groupCounts[pg.second];
+  for (const auto& g : groups) {
+    if (g != 0) {
+      ++groupCounts[g];
     }
   }
-  auto counts = std::vector<Num>{};
+  auto counts = std::vector<int>{};
   for (const auto& gc: groupCounts) {
     counts.push_back(gc.second);
   }
@@ -48,10 +44,10 @@ solve1(const M& m) {
 }
 
 static int
-isSingleGroup(const M& m) {
-  auto g = m.begin()->second;
-  for (const auto& pg : m) {
-    if (pg.second != g) {
+isSingleGroup(const Groups& groups) {
+  auto g = groups.front();
+  for (const auto& gg : groups) {
+    if (gg != g) {
       return false;
     }
   }
@@ -64,48 +60,45 @@ int
 main() {
   char c;
   Num x, y, z;
-  auto v = V{};
-  auto ps = PV{};
+  auto v = Junctions{};
   while (std::cin >> x >> c >> y >> c >> z) {
     v.push_back({x, y, z});
   }
-  const auto n = (v.size() < 100 ? 10UZ : 1000UZ);
+  const auto N = (v.size() < 100 ? 10UZ : 1000UZ);
 
+  auto tups = Tups{};
   for (int i = 0; i < v.size(); ++i) {
     for (int j = i + 1; j < v.size(); ++j) {
-      ps.push_back({v[i], v[j]});
+      tups.emplace_back(i, j, d2(v[i], v[j]));
     }
   }
-  std::sort(ps.begin(), ps.end(), [](const auto& a, const auto& b) { return d2(a.first, a.second) < d2(b.first, b.second); });
+  std::sort(tups.begin(), tups.end(), [](const auto& a, const auto& b) { return a.d2 < b.d2; });
 
-  auto m = M{};
-  for (const auto p : v) {
-    m[p] = 0;
-  }
+  auto groups = Groups(v.size(), 0);
   Num res1{}, res2{};
   int g = 0;
-  for (auto i = 0; i < ps.size(); ++i) {
-    const auto& p = ps[i];
-    if (i == n) {
-      res1 = solve1(m);
+  for (auto i = 0; i < tups.size(); ++i) {
+    const auto& t = tups[i];
+    if (i == N) {
+      res1 = solve1(groups);
     }
-    auto g1 = m[p.first];
-    auto g2 = m[p.second];
+    auto g1 = groups[t.a];
+    auto g2 = groups[t.b];
     if (g1 == 0 && g2 == 0) {
-      m[p.first] = m[p.second] = ++g;
+      groups[t.a] = groups[t.b] = ++g;
     } else if (g1 == 0) {
-      m[p.first] = g2;
+      groups[t.a] = g2;
     } else if (g2 ==0) {
-      m[p.second] = g1;
+      groups[t.b] = g1;
     } else {
-      for (auto& mv : m) {
-        if (mv.second == g2) {
-          mv.second = g1;
+      for (auto& og : groups) {
+        if (og == g2) {
+          og = g1;
         }
       }
     }
-    if (i > n && isSingleGroup(m)) {
-      res2 = p.first[0] * p.second[0];
+    if (i > N && isSingleGroup(groups)) {
+      res2 = v[t.a][0] * v[t.b][0];
       break;
     }
   }
