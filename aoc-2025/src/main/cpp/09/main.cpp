1d278ad3f4a141f543be265839f479b3607cb955
Péter Radics <mitchnull@gmail.com>
2025-12-09 20:08:31 +0100

    09: filter out (most?) 'outside' rectangles

--- aoc-2025/src/main/cpp/09/main.cpp
+++ aoc-2025/src/main/cpp/09/main.cpp
@@ -6,6 +6,31 @@ struct Line { Pos a, b; };
 using Lines = std::vector<Line>;
 
 static bool
+isInside(const Lines& lines, Pos p) {
+  int left{}, right{}, up{}, down{};
+  for (const auto& line: lines) {
+    if (line.a.x == line.b.x) {
+      if (std::min(line.a.y, line.b.y) <= p.y && p.y <= std::max(line.a.y, line.b.y)) {
+        if (line.a.x > p.x) {
+          ++right;
+        } else if (line.a.x < p.x) {
+          ++left;
+        }
+      }
+    } else {
+      if (std::min(line.a.x, line.b.x) <= p.x && p.x <= std::max(line.a.x, line.b.x)) {
+        if (line.a.y > p.y) {
+          ++down;
+        } else if (line.a.y < p.y) {
+          ++up;
+        }
+      }
+    }
+  }
+  return left % 2 == 1 || right % 2 == 1 || up % 2 == 1 || down % 2 == 1;
+}
+
+static bool
 check(const Lines& lines, Pos a, Pos b) {
   Pos tl{std::min(a.x, b.x), std::min(a.y, b.y)}, br{std::max(a.x, b.x), std::max(a.y, b.y)};
   for (const auto& line: lines) {
@@ -14,7 +39,10 @@ check(const Lines& lines, Pos a, Pos b) {
       return false;
     }
   }
-  return true;
+  return isInside(lines, {tl.x + 1, tl.y + 1})
+    && isInside(lines, {tl.x + 1, br.y - 1})
+    && isInside(lines, {br.x - 1, tl.y + 1})
+    && isInside(lines, {br.x - 1, br.y - 1});
 }
 
 /* ------------------------------------------------------------------------ */
