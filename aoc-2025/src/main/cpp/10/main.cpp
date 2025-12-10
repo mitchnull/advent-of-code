47f298c6c7fd3bd0f467acb4951cc409cd9c4b0f
Péter Radics <mitchnull@gmail.com>
2025-12-10 10:16:19 +0100

    10b sloooow

--- aoc-2025/src/main/cpp/10/main.cpp
+++ aoc-2025/src/main/cpp/10/main.cpp
@@ -2,12 +2,25 @@
 #include <sstream>
 #include <algorithm>
 #include <deque>
+#include <unordered_set>
 
 using Lights = std::bitset<16>;
+using V = std::vector<int>;
 struct Machine {
   Lights lights;
   std::vector<Lights> wirings;
-  std::vector<int> jolts;
+  V jolts;
+};
+
+template <>
+struct std::hash<V> {
+  std::size_t operator()(const V& jolts) const {
+    std::size_t h = 11;
+    for (const auto& v: jolts) {
+      h = hashCombine(h, v);
+    }
+    return h;
+  }
 };
 
 static int
@@ -22,6 +35,49 @@ solve1(const Machine& m) {
     for (auto w : m.wirings) {
       q.emplace_back(lights ^ w, step + 1);
     }
+  }
+  return -1;
+}
+
+static V
+charge(V jolts, Lights w) {
+  for (int i = 0; i < jolts.size(); ++i) {
+    if (w[i]) {
+      ++jolts[i];
+    }
+  }
+  return jolts;
+}
+
+static bool
+isOvercharged(const Machine& m, const V& jolts) {
+  for (int i = 0; i < jolts.size(); ++i) {
+    if (jolts[i] > m.jolts[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+static int
+solve2(const Machine& m) {
+  println("@@@ checking: m: {}", m.jolts);
+  auto wirings = std::vector<Lights>{};
+  auto ways = std::unordered_set<V>{V(m.jolts.size(), 0)};
+  for (int res = 1; ; ++res) {
+    auto next = std::unordered_set<V>();
+    for (auto pj : ways) {
+      for (auto w : m.wirings) {
+        auto jolts = charge(pj, w);
+        if (jolts == m.jolts) {
+          return res;
+        }
+        if (!isOvercharged(m, jolts)) {
+          next.insert(jolts);
+        }
+      }
+    }
+    std::swap(ways, next);
   }
   return -1;
 }
@@ -43,7 +99,7 @@ main() {
       }
     }
     std::vector<Lights> wirings;
-    std::vector<int> jolts;
+    V jolts;
     while (line >> str) {
       std::istringstream ss{str};
       char ch;
@@ -66,9 +122,10 @@ main() {
   }
 
   int res1 = ranges::fold_left(machines | views::transform([](const auto& m) { return solve1(m); }), 0, std::plus<>());
+  int res2 = ranges::fold_left(machines | views::transform([](const auto& m) { return solve2(m); }), 0, std::plus<>());
 
   println("1: {}", res1);
-  println("2: {}", 0);
+  println("2: {}", res2);
 
   return 0;
 }
