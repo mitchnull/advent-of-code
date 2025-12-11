6b2a8a687fd8ce2e59c45132382fcfdbbf2c5fc0
Péter Radics <mitchnull@gmail.com>
2025-12-11 14:56:37 +0100

    10b start from scratch

--- aoc-2025/src/main/cpp/10/main.cpp
+++ aoc-2025/src/main/cpp/10/main.cpp
@@ -2,25 +2,16 @@
 #include <sstream>
 #include <algorithm>
 #include <deque>
+#include <bitset>
 #include <unordered_set>
 
-using Lights = std::uint16_t;
+using Lights = std::bitset<16>;
 using V = std::vector<std::int16_t>;
+
 struct Machine {
   Lights lights;
   std::vector<Lights> wirings;
   V jolts;
-};
-
-template <>
-struct std::hash<V> {
-  std::size_t operator()(const V& jolts) const {
-    std::size_t h = 11;
-    for (const auto& v: jolts) {
-      h = hashCombine(h, v);
-    }
-    return h;
-  }
 };
 
 static int
@@ -39,56 +30,8 @@ solve1(const Machine& m) {
   return -1;
 }
 
-static inline bool
-isSet(Lights w, int i) {
-  return w & (1 << i);
-}
-
-static inline void
-set(Lights& w, int i) {
-  w |= (1 << i);
-}
-
-static V
-charge(V jolts, Lights w) {
-  for (int i = 0; i < jolts.size(); ++i) {
-    if (isSet(w, i)) {
-      ++jolts[i];
-    }
-  }
-  return jolts;
-}
-
-static bool
-isOvercharged(const Machine& m, const V& jolts) {
-  for (int i = 0; i < jolts.size(); ++i) {
-    if (jolts[i] > m.jolts[i]) {
-      return true;
-    }
-  }
-  return false;
-}
-
 static int
 solve2(const Machine& m) {
-  println("@@@ checking: {}", m.wirings);
-  auto ways = std::unordered_set<V>{V(m.jolts.size(), 0)};
-  for (int res = 1; ; ++res) {
-    auto next = std::unordered_set<V>();
-    for (const auto& pj : ways) {
-      for (auto w : m.wirings) {
-        auto jolts = charge(pj, w);
-        if (jolts == m.jolts) {
-          println("@@@ res {}: {}", m.jolts, res);
-          return res;
-        }
-        if (!isOvercharged(m, jolts)) {
-          next.insert(jolts);
-        }
-      }
-    }
-    std::swap(ways, next);
-  }
   return -1;
 }
 
@@ -105,7 +48,7 @@ main() {
     Lights lights{};
     for (int i = 1; i < str.size() - 1; ++i) {
       if (str[i] == '#') {
-        set(lights, i - 1);
+        lights[i - 1] = true;
       }
     }
     std::vector<Lights> wirings;
@@ -118,7 +61,7 @@ main() {
       if (ch == '(') {
         Lights wiring{};
         while (ss >> button >> ch) {
-          set(wiring, button);
+          wiring[button] = true;
         }
         wirings.push_back(wiring);
       } else {
