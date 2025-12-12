1543143545487083f4cfa5ed18b2506cb6a4b746
Péter Radics <mitchnull@gmail.com>
2025-12-12 10:10:05 +0100

    12: stripped down min-max version

--- aoc-2025/src/main/cpp/12/main.cpp
+++ aoc-2025/src/main/cpp/12/main.cpp
@@ -4,163 +4,27 @@
 #include <sstream>
 #include <iterator>
 
-using Num = int64_t;
-using Box = Grid<>;
-using Boxes = std::vector<Box>;
-using Region = Grid<>;
 struct Task {
   int w, h;
   std::vector<int> items;
 };
-using Tasks = std::vector<Task>;
-using BoxConfigs = std::vector<Boxes>;
 
-struct State {
-  int item, x = 0, y = 0, conf = 0;
-};
-using States = std::vector<State>;
-
-template <>
-struct std::formatter<State> {
-  constexpr auto parse(std::format_parse_context& ctx) {
-    return ctx.begin();
-  }
-
-  auto format(const State& s, std::format_context& ctx) const {
-    return std::format_to(ctx.out(), "{{{}, {}, {}, {}}}", s.item, s.x, s.y, s.conf);
-  }
-};
-
-template <>
-struct std::formatter<States> {
-  constexpr auto parse(std::format_parse_context& ctx) {
-      return ctx.begin();
-  }
-
-  auto format(const States& p, std::format_context& ctx) const {
-    std::format_to(ctx.out(), "[");
-    std::string sep = "";
-    for (auto s : p) {
-      std::format_to(ctx.out(), "{}{}", sep, s);
-      sep = ", ";
-    }
-    return std::format_to(ctx.out(), "]");
-  }
-};
-
-static Box
-rot(const Box& b) {
-  Box r(b.h(), b.w(), b[-1, -1], b[-1, -1]);
-  for (int y = 0; y < b.h(); ++y) {
-    for (int x = 0; x < b.w(); ++x) {
-      r[y, r.h() - x - 1] = b[x, y];
-    }
-  }
-  return r;
-}
-
-static Box
-flip(const Box& b) {
-  Box r(b.w(), b.h(), b[-1, -1], b[-1, -1]);
-  for (int y = 0; y < b.h(); ++y) {
-    for (int x = 0; x < b.w(); ++x) {
-      r[r.w() - x - 1,  y] = b[x, y];
-    }
-  }
-  return r;
-}
-
-static Boxes
-rotFlip(Box b) {
-  Boxes res{b};
-  for (int i = 0; i < 3; ++i) {
-    b = rot(b);
-    if (std::find(res.begin(), res.end(), b) == res.end()) {
-      res.push_back(b);
-    }
-  }
-  b = flip(rot(b));
-  if (std::find(res.begin(), res.end(), b) == res.end()) {
-    res.push_back(b);
-  }
-  for (int i = 0; i < 3; ++i) {
-    b = rot(b);
-    if (std::find(res.begin(), res.end(), b) == res.end()) {
-      res.push_back(b);
-    }
-  }
-  return res;
-}
-
-static bool
-check(const Task& t, const BoxConfigs& boxConfigs, const States& p) {
-  Region r(t.w, t.h, '.');
-  for (const auto &s : p) {
-    const auto& b = boxConfigs[s.item][s.conf];
-    for (int y = 0; y < b.h(); ++y) {
-      for (int x = 0; x < b.w(); ++x) {
-        if (b[x, y] == '#' && r[s.x + x, s.y + y] == '#') {
-          return false;
-        }
-        r[s.x + x, s.y + y] = b[x, y];
-      }
-    }
-  }
-  return true;
-}
-
-static bool
-inc(const Task& t, const BoxConfigs& boxConfigs, States& p) {
-  for (auto& s : p) {
-    const auto& b = boxConfigs[s.item][s.conf];
-    if (++s.x <= t.w - b.w()) {
-      return true;
-    }
-    s.x = 0;
-    if (++s.y <= t.h - b.h()) {
-      return true;
-    }
-    s.y = 0;
-    if (++s.conf < boxConfigs[s.item].size()) {
-      return true;
-    }
-    s.conf = 0;
-  }
-  return false;
-}
-
-static bool
-solve1(const Task& t, const BoxConfigs& boxConfigs, const std::vector<int>& boxSizes) {
+static std::pair<int, int>
+solve1(const Task& t, const std::vector<int>& blockFills, int ext) {
   const int N = t.items.size();
+  int numItems = std::reduce(t.items.begin(), t.items.end(), 0, std::plus<>{});
+  int easyPlaces = (t.w / ext) * (t.h / ext);
+  if (numItems <= easyPlaces) {
+    return {1, 1}; // yep
+  }
   int bs = 0;
   for (int i = 0; i < N; ++i) {
-    bs += t.items[i] * boxSizes[i];
+    bs += t.items[i] * blockFills[i];
   }
   if (bs > (t.w * t.h)) {
-    return false;
+    return {0, 0}; // nope
   }
-  int ext = 0;
-  for (const auto& bc : boxConfigs) {
-    ext = std::max(ext, std::max(bc[0].w(), bc[0].h()));
-  }
-  int easy = (t.w / ext) * (t.h / ext);
-  int sum = std::reduce(t.items.begin(), t.items.end(), 0, std::plus<>{});
-  if (sum <= easy) {
-    return true;
-  }
-
-  std::vector<State> p;
-  for (int i = 0; i < N; ++i) {
-    for (int j = 0; j < t.items[i]; ++j) {
-      p.push_back(State{i});
-    }
-  }
-  do {
-    if (check(t, boxConfigs, p)) {
-      return true;
-    }
-  } while (inc(t, boxConfigs, p));
-  return false;
+  return {0, 1}; // maybe...
 }
 
 /* ------------------------------------------------------------------------ */
@@ -169,32 +33,37 @@ solve1(const Task& t, const BoxConfigs& boxConfigs, const std::vector<int>& boxS
 int
 main() {
   std::string line;
-  Boxes boxes;
-  Tasks tasks;
+  std::vector<int> blockFills;
+  std::vector<Task> tasks;
+  auto ext = 0UZ;
   while (std::getline(std::cin, line)) {
     if (line.empty()) {
       continue;
     }
     std::istringstream ss{line};
-    int w, h;
+    int w;
     char ch;
     ss >> w >> ch;
     if (ch == ':') {
-      std::vector<std::string> lines;
+      auto h = 0UZ;
+      int fill = 0;
       while (std::getline(std::cin, line) && !line.empty()) {
-        lines.push_back(line);
+        fill += ranges::count(line, '#');
+        ext = std::max(ext, line.length());
+        ++h;
       }
-      boxes.push_back(Box(lines, '.'));
+      ext = std::max(ext, h);
+      blockFills.push_back(fill);
     } else {
+      int h;
       ss >> h >> ch;
       std::vector<int> items{std::istream_iterator<int>{ss}, std::istream_iterator<int>()};
       tasks.emplace_back(w, h, items);
     }
   }
-  BoxConfigs boxConfigs = boxes | views::transform(rotFlip) | ranges::to<BoxConfigs>();
-  auto boxSizes = boxes | views::transform([](auto b) { return std::count(b.begin(), b.end(), '#'); }) | ranges::to<std::vector<int>>();
-
-  auto res1 = std::transform_reduce(tasks.begin(), tasks.end(), 0, std::plus<>{}, [&](const auto& t) { return solve1(t, boxConfigs, boxSizes); });
+  auto res1 = std::transform_reduce(tasks.begin(), tasks.end(), std::make_pair(0, 0),
+      [](auto a, auto b) { return a + b; },
+      [&](const auto& t) { return solve1(t, blockFills, ext); });
   println("1: {}", res1);
 
   return 0;
