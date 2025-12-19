49fc9eb07dcdcc0c7339084b810449a6c6b823d6
Péter Radics <mitchnull@gmail.com>
2025-12-19 16:54:11 +0100

    search over junctions

--- aoc-2023/src/main/cpp/23/main.cpp
+++ aoc-2023/src/main/cpp/23/main.cpp
@@ -1,4 +1,5 @@
 #include "../utils.h"
+#include <unordered_map>
 
 using Num = int;
 using Board = Grid<>;
@@ -34,10 +35,21 @@ solve1(const Board &b, const Pos &dst, Pos pos = {1, 0}, int dir = 2, Num len = 
   return res;
 }
 
-static Num
-solve2(const Board &b, const Pos &dst, Path &path, Pos pos = {1, 0}, int dir = 2, Num len = 0) {
+struct Edge {
+  Pos pos;
+  Num len;
+};
+
+/* ------------------------------------------------------------------------ */
+
+using Edges = std::vector<Edge>;
+using Graph = std::unordered_map<Pos, Edges>;
+
+static void
+junctions(const Board &b, const Pos &dst, Graph &g, Pos src, Pos pos, int dir, Num len) {
   if (pos == dst) {
-    return len;
+    g[src].emplace_back(pos, len);
+    return;
   }
   std::vector<int> ways;
   for (int d = 0; d < 4; ++d) {
@@ -49,23 +61,46 @@ solve2(const Board &b, const Pos &dst, Path &path, Pos pos = {1, 0}, int dir = 2
     }
   }
   if (ways.empty()) {
-    return 0;
+    return;
   }
   if (ways.size() > 1) {
-    if (std::find(path.begin(), path.end(), pos) != path.end()) {
-      return 0;
+    if (std::find_if(g[pos].begin(), g[pos].end(), [&](auto e) { return e.pos == src; }) != g[pos].end()) {
+      return;
     }
-    path.push_back(pos);
+    g[src].emplace_back(pos, len);
+    g[pos].emplace_back(src, len);
+    src = pos;
+    len = 0;
   }
-  Num res = 0;
   for (auto d : ways) {
     Pos np = pos + DIRS[d];
-    res = std::max(res, solve2(b, dst, path, np, d, len + 1));
+    junctions(b, dst, g, src, np, d, len + 1);
   }
-  if (ways.size() > 1) {
+}
+
+static Num
+solveJunctions(const Graph &g, const Pos &dst, Path &path, Pos pos, Num len) {
+  if (pos == dst) {
+    return len;
+  }
+  Num res = 0;
+  for (const auto &e : g.at(pos)) {
+    if (std::find(path.begin(), path.end(), e.pos) != path.end()) {
+      continue;
+    }
+    path.push_back(e.pos);
+    res = std::max(res, solveJunctions(g, dst, path, e.pos, len + e.len));
     path.pop_back();
   }
   return res;
+}
+
+static Num
+solve2(const Board &b, const Pos &dst, const Pos &src = {1, 0}, int dir = 2) {
+  Graph g;
+  junctions(b, dst, g, src, src, dir, 0);
+  Path path;
+  return solveJunctions(g, dst, path, src, 0);
 }
 
 /* ------------------------------------------------------------------------ */
@@ -77,6 +112,6 @@ main() {
   Pos dst{board.w() - 2, board.h() - 1};
   println("1: {}", solve1(board, dst));
   Path path;
-  println("2: {}", solve2(board, dst, path));
+  println("2: {}", solve2(board, dst));
   return 0;
 }
