c8db7e54c5f5e83a1f3b45b37904c9a45901476d
Péter Radics <mitchnull@gmail.com>
2025-12-19 13:57:04 +0100

    23b slooow

--- aoc-2023/src/main/cpp/23/main.cpp
+++ aoc-2023/src/main/cpp/23/main.cpp
@@ -13,7 +13,6 @@ struct Node {
 
 struct Node2 : public Node {
   Path path;
-  Path fullPath;
 };
 
 static const auto DIR_SYMS = "^>v<";
@@ -46,12 +45,10 @@ static Num
 solve2(const Board &b) {
   Num res = 0;
   Pos dst = {b.w() - 2, b.h() - 1};
-  std::deque<Node2> q{{{Pos{1, 0}, 2, 0}, {}}};
-  println("{}", b); // @@@
+  std::deque<Node2> q{{{Pos{1, 0}, 2, 0}}};
   while (!q.empty()) {
     auto n = q.front();
     q.pop_front();
-    println("@@@ n: {}, {}, {}", n.pos, DIR_SYMS[n.dir], n.len);
     if (n.pos == dst) {
       res = std::max(res, n.len);
     }
@@ -70,69 +67,13 @@ solve2(const Board &b) {
     Path path = n.path;
     if (ways.size() > 1) {
       if (std::find(path.begin(), path.end(), n.pos) != path.end()) {
-        println("@@@ skipping junction at {}, path: {}", n.pos, n.path);
-        auto bb = b;
-        auto c = '0';
-        for (auto p : n.path) {
-          bb[p] = c;
-          if (c == '9') {
-            c = 'A';
-          } else if (c == 'Z') {
-            c = 'a';
-          } else {
-            ++c;
-          }
-        }
-        println("{}", bb);
         continue;
       }
-      println("@@@ junction at {}, path: {}", n.pos, n.path);
       path.push_back(n.pos);
-    }
-    Path fullPath = n.fullPath;
-    if (std::find(fullPath.begin(), fullPath.end(), n.pos) != fullPath.end()) {
-      println("@@@ loop at {}, {}", n.pos, n.fullPath);
-      auto bb = b;
-      for (auto p : n.fullPath) {
-        bb[p] = 'o';
-      }
-      auto c = '0';
-      for (auto p : n.path) {
-        bb[p] = c;
-        if (c == '9') {
-          c = 'a';
-        } else if (c == 'z') {
-          c = 'a';
-        } else {
-          ++c;
-        }
-      }
-      println("{}", bb);
-      abort();
-    }
-    fullPath.push_back(n.pos);
-    println("@@@ ways: {}", ways);
-    {
-      auto bb = b;
-      for (auto p : n.fullPath) {
-        bb[p] = 'o';
-      }
-      auto c = '0';
-      for (auto p : n.path) {
-        bb[p] = c;
-        if (c == '9') {
-          c = 'a';
-        } else if (c == 'z') {
-          c = 'a';
-        } else {
-          ++c;
-        }
-      }
-      println("{}", bb);
     }
     for (auto d : ways) {
       Pos np = n.pos + DIRS[d];
-      q.push_back({{np, d, n.len + 1}, path, fullPath});
+      q.push_back({{np, d, n.len + 1}, path});
     }
   }
   return res;
