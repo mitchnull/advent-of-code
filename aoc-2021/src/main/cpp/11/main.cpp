d5e3efc0f724a54f60f193a3d5bd18440bd56b9f
Péter Radics <mitchnull@gmail.com>
2025-12-22 12:58:56 +0100

    11 init

---
+++ aoc-2021/src/main/cpp/11/main.cpp
@@ -0,0 +1,15 @@
+#include "../utils.h"
+
+using Num = int64_t;
+
+/* ------------------------------------------------------------------------ */
+
+int
+main() {
+  Num res1{}, res2{};
+
+  println("1: {}", res1);
+  println("2: {}", res2);
+
+  return 0;
+}
