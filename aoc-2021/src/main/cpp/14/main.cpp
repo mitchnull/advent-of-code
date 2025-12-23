7e29d75ba00c4859a10f2838454dac3b720d845f
Péter Radics <mitchnull@gmail.com>
2025-12-23 12:31:00 +0100

    14

--- aoc-2021/src/main/cpp/14/main.cpp
+++ aoc-2021/src/main/cpp/14/main.cpp
@@ -2,37 +2,56 @@
 #include <algorithm>
 #include <unordered_map>
 
-struct Pair {
-  char f, s;
-  friend auto operator<=>(const Pair &, const Pair &) = default;
-};
+using Pair = std::pair<char, char>;
+using Num = int64_t;
+using PT = std::unordered_map<Pair, char>;
+using Hist = std::unordered_map<char, Num>;
+using Counts = std::unordered_map<Pair, Num>;
 
 template <>
 struct std::hash<Pair> {
-  std::size_t operator()(const Pair &p) const { return hashCombine(p.f, p.s); }
+  std::size_t operator()(const Pair &p) const { return hashCombine(p.first, p.second); }
 };
 
-using Num = int64_t;
-using PT = std::unordered_map<Pair, char>;
-using Hist = std::unordered_map<char, int>;
-
 static Num
-solve1(const PT &pt, string poly) {
-  for (int i = 0; i < 10; ++i) {
-    for (auto it = poly.begin(); it != poly.end() - 1; ++it) {
-      auto pp = pt.find({*it, *(it + 1)});
-      if (pp != pt.end()) {
-        it = poly.insert(it + 1, pp->second);
-      }
-    }
-  }
+result(const Counts &counts, string poly) {
   Hist h;
-  for (char c : poly) {
-    ++h[c];
+  for (auto e : counts) {
+    h[e.first.first] += e.second;
+    h[e.first.second] += e.second;
+  }
+  for (auto &he : h) {
+    if (he.first == poly.front() || he.first == poly.back()) {
+      he.second = (he.second / 2) + 1;
+    } else {
+      he.second = (he.second + 1) / 2;
+    }
   }
   auto [minIt, maxIt] = std::minmax_element(h.begin(), h.end(), [](auto a, auto b) { return a.second < b.second; });
   return maxIt->second - minIt->second;
 }
+
+static std::pair<Num, Num>
+solve(const PT &pt, string poly) {
+  Num res1 = 0;
+  Counts counts;
+  for (auto it = poly.begin(); it != poly.end() - 1; ++it) {
+    ++counts[{*it, *(it + 1)}];
+  }
+  for (int i = 0; i < 40; ++i) {
+    if (i == 10) {
+      res1 = result(counts, poly);
+    }
+    Counts next;
+    for (auto e : counts) {
+      char ins = pt.at(e.first);
+      next[{e.first.first, ins}] += e.second;
+      next[{ins, e.first.second}] += e.second;
+    }
+    counts = std::move(next);
+  }
+  return {res1, result(counts, poly)};
+};
 
 /* ------------------------------------------------------------------------ */
 
@@ -45,6 +64,9 @@ main() {
   while (std::cin >> f >> s >> str >> ins) {
     pt[{f, s}] = ins;
   }
-  println("1: {}", solve1(pt, poly));
+  auto [res1, res2] = solve(pt, poly);
+  println("1: {}", res1);
+  println("2: {}", res2);
+
   return 0;
 }
