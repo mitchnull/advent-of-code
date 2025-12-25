fa470753a719f9892762c1da7b34064a6058a91d
Péter Radics <mitchnull@gmail.com>
2025-12-25 11:11:13 +0100

    17

--- aoc-2021/src/main/cpp/17/main.cpp
+++ aoc-2021/src/main/cpp/17/main.cpp
@@ -1,19 +1,15 @@
 #include "../utils.h"
 #include <regex>
 
-// y = (2 * y0 - t + 1) * t / 2;
-// (2 * y) / t = 2 * y0 - t + 1
-// ((2 * y) / t + t - 1) / 2 = y0
-
 static int
 height(int y) {
-  int tm = (2 * y + 1) / 2;
+  int tm = std::max(0, (2 * y + 1) / 2);
   return (2 * y - tm + 1) * tm / 2;
 }
 
 static bool
 check(const Pos &tl, const Pos &br, Dir d) {
-  for (Pos p = {0, 0}; p.y >= br.y; p += d, d = { std::max(0, d.dx - 1), d.dy - 1 }) {
+  for (Pos p = {0, 0}; p.y >= br.y; p += d, d = {std::max(0, d.dx - 1), d.dy - 1}) {
     if (tl.x <= p.x && p.x <= br.x && tl.y >= p.y && p.y >= br.y) {
       return true;
     }
@@ -21,22 +17,23 @@ check(const Pos &tl, const Pos &br, Dir d) {
   return false;
 }
 
-static int
-solve1(Pos tl, Pos br) {
-  int res = 0;
+static std::pair<int, int>
+solve(Pos tl, Pos br) {
+  int res1 = 0, res2 = 0;
   if ((tl.x < 0 && br.x < 0) || (tl.x < 0 && -tl.x < br.x)) {
     std::tie(tl.x, br.x) = std::make_pair(-br.x, -tl.x);
   }
   int mx = std::max(std::abs(tl.x), std::abs(br.x));
   int my = std::max(std::abs(tl.y), std::abs(br.y));
-  for (int y = 0; y < my; ++y) {
-    for (int x = 0; x < mx; ++x) {
+  for (int y = -my; y <= my; ++y) {
+    for (int x = -mx; x <= mx; ++x) {
       if (check(tl, br, {x, y})) {
-        res = std::max(res, height(y));
+        res1 = std::max(res1, height(y));
+        ++res2;
       }
     }
   }
-  return res;
+  return {res1, res2};
 }
 
 /* ------------------------------------------------------------------------ */
@@ -53,7 +50,9 @@ main() {
   auto ymax = std::stoi(it++->str());
   auto ymin = std::stoi(it++->str());
 
-  println("1: {}", solve1({xmin, ymin}, {xmax, ymax}));
+  auto [res1, res2] = solve({xmin, ymin}, {xmax, ymax});
+  println("1: {}", res1);
+  println("2: {}", res2);
 
   return 0;
 }
