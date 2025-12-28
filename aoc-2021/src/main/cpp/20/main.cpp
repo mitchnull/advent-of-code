67a08450ecf01605b601bdd661f514882240c6cc
Péter Radics <mitchnull@gmail.com>
2025-12-28 11:32:06 +0100

    20a

---
+++ aoc-2021/src/main/cpp/20/main.cpp
@@ -0,0 +1,41 @@
+#include "../utils.h"
+#include <algorithm>
+
+using Num = int64_t;
+using Image = Grid<>;
+
+static Num
+solve1(Image image, const string &mapping, int count) {
+  for (int i = 0; i < count; ++i) {
+    Image next = Image(image.w() + 2, image.h() + 2, {}, image[-1, -1] == '.' ? mapping.front() : mapping.back());
+    for (int y = 0; y < next.h(); ++y) {
+      for (int x = 0; x < next.w(); ++x) {
+        int bin = 0;
+        for (int dy = -1; dy <= 1; ++dy) {
+          for (int dx = -1; dx <= 1; ++dx) {
+            bin = (bin << 1) + (image[x + dx - 1, y + dy - 1] == '#');
+          }
+        }
+        next[x, y] = mapping[bin];
+      }
+    }
+    image = std::move(next);
+  }
+  return ranges::count(image, '#');
+}
+
+/* ------------------------------------------------------------------------ */
+
+int
+main() {
+  string mapping;
+  string line;
+  while (std::getline(std::cin, line) && !line.empty()) {
+    mapping += line;
+  }
+  Image image = Image::read(std::cin, '.');
+
+  println("1: {}", solve1(image, mapping, 2));
+
+  return 0;
+}
