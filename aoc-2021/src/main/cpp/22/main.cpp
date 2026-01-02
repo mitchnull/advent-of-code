1f1258bf229d7701cdad8a3c64869541369fb389
Péter Radics <mitchnull@gmail.com>
2026-01-02 15:25:39 +0100

    cleanup

--- aoc-2021/src/main/cpp/22/main.cpp
+++ aoc-2021/src/main/cpp/22/main.cpp
@@ -1,15 +1,13 @@
 #include "../utils.h"
-#include <algorithm>
+#include <numeric>
 #include <regex>
-#include <valarray>
 
-constexpr const int N = 3;
 using Num = int64_t;
-using V = std::valarray<Num>;
+using V = std::vector<Num>;
 
 struct Cube {
   V bl, tr;
-  bool on = false;
+  bool on;
 };
 using Cubes = std::vector<Cube>;
 
@@ -28,7 +26,7 @@ struct std::formatter<Cube> {
 
 static bool
 isColliding(const Cube &a, const Cube &b) {
-  for (int i = 0; i < N; ++i) {
+  for (int i = 0; i < a.bl.size(); ++i) {
     if (b.tr[i] < a.bl[i] || a.tr[i] < b.bl[i]) {
       return false;
     }
@@ -44,21 +42,17 @@ split(const Cubes &core, Cube c) {
       next.push_back(std::move(cc));
       continue;
     }
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < c.bl.size(); ++i) {
       if (cc.bl[i] < c.bl[i]) {
-        // println("@@@ split left: {}:", cc);
         Cube lc = cc;
         lc.tr[i] = c.bl[i] - 1;
         cc.bl[i] = c.bl[i];
-        // println("@@@ after split left ({}): {} + {}:", i, lc, cc);
         next.push_back(std::move(lc));
       }
       if (cc.tr[i] > c.tr[i]) {
-        // println("@@@ split right: {}:", cc);
         Cube rc = cc;
         rc.bl[i] = c.tr[i] + 1;
         cc.tr[i] = c.tr[i];
-        // println("@@@ after split right ({}): {} + {}:", i, cc, rc);
         next.push_back(std::move(rc));
       }
     }
@@ -69,8 +63,8 @@ split(const Cubes &core, Cube c) {
 static bool
 isBootArea(const Cube &c) {
   for (auto w : {&Cube::bl, &Cube::tr}) {
-    for (int i = 0; i < N; ++i) {
-      if ((c.*w)[i] < -50 || (c.*w)[i] > 50) {
+    for (auto v : c.*w) {
+      if (v < -50 || v > 50) {
         return false;
       }
     }
@@ -78,38 +72,19 @@ isBootArea(const Cube &c) {
   return true;
 }
 
-static void
-debug(const Cubes &core) {
-  const int MIN = 0, MAX = 13;
-  for (int i = MIN; i <= MAX; ++i) {
-    for (int j = MIN; j <= MAX; ++j) {
-      for (int k = MIN; k <= MAX; ++k) {
-        for (const auto &c : core) {
-          if (isColliding(c, Cube{{i, j, k}, {i, j, k}, false})) {
-            println("- {},{},{}", i, j, k);
-          }
-        }
-      }
-    }
-  }
-}
-
 static std::pair<Num, Num>
 solve(const Cubes &input) {
   Cubes core;
   for (const auto &c : input) {
-    // println("@@@ splitting by {}, {}", c, core.size());
     core = split(core, c);
     if (c.on) {
       core.push_back(c);
     }
-    // println("@@@ after splitting: {}", core);
-    // debug(core); // @@@
   }
   Num res1 = 0, res2 = 0;
   for (const auto &c : core) {
-    V d = (c.tr - c.bl) + 1;
-    Num v = ranges::fold_left(d, Num{1}, std::multiplies<>{});
+    Num v = std::inner_product(
+        begin(c.bl), end(c.bl), begin(c.tr), Num{1}, std::multiplies<>(), [](auto a, auto b) { return b - a + 1; });
     if (isBootArea(c)) {
       res1 += v;
     }
@@ -128,16 +103,12 @@ main() {
   string line;
   std::regex re("([0-9-]+)");
   while (std::getline(std::cin, line)) {
-    auto it = std::sregex_iterator(line.begin(), line.end(), re);
-    Num x1 = std::stoi(it++->str());
-    Num x2 = std::stoi(it++->str());
-    Num y1 = std::stoi(it++->str());
-    Num y2 = std::stoi(it++->str());
-    Num z1 = std::stoi(it++->str());
-    Num z2 = std::stoi(it++->str());
-    cubes.emplace_back(V{std::min(x1, x2), std::min(y1, y2), std::min(z1, z2)},
-        V{std::max(x1, x2), std::max(y1, y2), std::max(z1, z2)},
-        line[1] == 'n');
+    std::vector<Num> bl, tr;
+    for (auto it = std::sregex_iterator(line.begin(), line.end(), re); it != std::sregex_iterator();) {
+      bl.push_back(std::stoi(it++->str()));
+      tr.push_back(std::stoi(it++->str()));
+    }
+    cubes.emplace_back(bl, tr, line[1] == 'n');
   }
   auto [res1, res2] = solve(cubes);
   println("1: {}", res1);
