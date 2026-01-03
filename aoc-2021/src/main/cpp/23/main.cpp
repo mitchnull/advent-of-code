a17df05813d98f0f7381156f6d02cd9d1b90c811
Péter Radics <mitchnull@gmail.com>
2026-01-03 15:44:46 +0100

    minor speedup

--- aoc-2021/src/main/cpp/23/main.cpp
+++ aoc-2021/src/main/cpp/23/main.cpp
@@ -4,19 +4,13 @@
 #include <unordered_map>
 
 using Num = int;
-using Board = Grid<>;
 
 static const auto Costs = std::vector<Num>{1, 10, 100, 1000};
 static const auto HallwayPlaces =
     std::vector<Pos>{Pos{1, 1}, Pos{2, 1}, Pos{4, 1}, Pos{6, 1}, Pos{8, 1}, Pos{10, 1}, Pos{10, 1}, Pos{11, 1}};
-static const auto HomePlaces = std::vector<std::vector<Pos>>{
-    {Pos{3, 2}, Pos{3, 3}, Pos{3, 4}, Pos{3, 5}},
-    {Pos{5, 2}, Pos{5, 3}, Pos{5, 4}, Pos{5, 5}},
-    {Pos{7, 2}, Pos{7, 3}, Pos{7, 4}, Pos{7, 5}},
-    {Pos{9, 2}, Pos{9, 3}, Pos{9, 4}, Pos{9, 5}}};
-static const auto ExtraLines = std::vector<string> {
-"  #D#C#B#A#  ",
-"  #D#B#A#C#  ",
+static const auto ExtraLines = std::vector<string>{
+    "  #D#C#B#A#  ",
+    "  #D#B#A#C#  ",
 };
 
 using Places = std::array<std::vector<Pos>, 4>;
@@ -46,10 +40,14 @@ using Queue = std::priority_queue<Node>;
 
 /* ------------------------------------------------------------------------ */
 
+static Pos
+homePlace(int i, int j) {
+  return {i * 2 + 3, j + 2};
+}
+
 static bool
 isHome(const Places &p, int i, int j) {
-  const auto &h = HomePlaces[i];
-  return std::find(begin(h), end(h), p[i][j]) != h.end();
+  return p[i][j].y > 1 && p[i][j].x == (i * 2 + 3);
 }
 
 static bool
@@ -170,9 +168,9 @@ solve(const std::vector<string> lines) {
       int hp = p[i].size();
       for (int j = 0; j < hp; ++j) {
         if (isHallway(p[i][j])) {
-          if (!tryMove(q, dists, dist, p, i, j, HomePlaces[i][hp - 1])) {
+          if (!tryMove(q, dists, dist, p, i, j, homePlace(i, hp - 1))) {
             for (int k = hp - 1; k > 0; --k) {
-              if (at(p, HomePlaces[i][k]) != i || tryMove(q, dists, dist, p, i, j, HomePlaces[i][k - 1])) {
+              if (at(p, homePlace(i, k)) != i || tryMove(q, dists, dist, p, i, j, homePlace(i, k - 1))) {
                 break;
               }
             }
