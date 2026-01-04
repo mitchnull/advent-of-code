ebd36a5992cb6a3cd432ed2578963ff4dc8a0f88
Péter Radics <mitchnull@gmail.com>
2026-01-04 20:05:10 +0100

    remove fluff

--- aoc-2021/src/main/cpp/24/main.cpp
+++ aoc-2021/src/main/cpp/24/main.cpp
@@ -1,98 +1,12 @@
 #include "../utils.h"
 #include <sstream>
-#include <variant>
 
 using Num = int64_t;
 
 struct Block {
   int div, dx, dy;
 };
-
-template <>
-struct std::formatter<Block> {
-  template <typename FormatContext>
-  constexpr auto parse(FormatContext &ctx) {
-    return ctx.begin();
-  }
-
-  template <typename FormatContext>
-  constexpr auto format(const Block &b, FormatContext &ctx) const {
-    return format_to(ctx.out(), "{{div={}, dx={}, dy={}}}", b.div, b.dx, b.dy);
-  }
-};
-
 using Blocks = std::vector<Block>;
-
-enum Mnemoic {
-  INP,
-  ADD,
-  MUL,
-  DIV,
-  MOD,
-  EQL,
-};
-
-static string
-name(Mnemoic m) {
-  switch (m) {
-    case INP: return "inp";
-    case ADD: return "add";
-    case MUL: return "mul";
-    case DIV: return "div";
-    case MOD: return "mod";
-    case EQL: return "eql";
-  }
-}
-
-using Op = std::variant<char, Num>;
-
-static string
-to_string(Op op) {
-  return std::holds_alternative<Num>(op) ? std::to_string(std::get<Num>(op)) : string{std::get<char>(op)};
-}
-
-struct Inst {
-  Mnemoic m;
-  char op1;
-  Op op2;
-};
-
-using Prog = std::vector<Inst>;
-using Regs = std::vector<Num>;
-
-static Op
-op(string v) {
-  return std::isalpha(v[0]) ? Op(v[0]) : std::stol(v);
-}
-
-static Num
-v(const Regs &r, Op op) {
-  if (std::holds_alternative<Num>(op)) {
-    return std::get<Num>(op);
-  }
-  return r[std::get<char>(op)];
-}
-
-static bool
-eval(const Prog &prog, Num n) {
-  auto r = Regs(256, 0);
-  string input = std::to_string(n);
-  if (std::find(begin(input), end(input), '0') != end(input)) {
-    return false;
-  }
-  auto it = input.begin();
-  for (const auto &i : prog) {
-    switch (i.m) {
-      case INP: r[i.op1] = (*it++ - '0'); break;
-      case ADD: r[i.op1] += v(r, i.op2); break;
-      case MUL: r[i.op1] *= v(r, i.op2); break;
-      case DIV: r[i.op1] /= v(r, i.op2); break;
-      case MOD: r[i.op1] %= v(r, i.op2); break;
-      case EQL: r[i.op1] = (r[i.op1] == v(r, i.op2)); break;
-    }
-  }
-  return r['z'] == 0;
-}
 
 static Num
 solve1(const Blocks &blocks, Num n = 0, int z = 0, int i = 0) {
@@ -120,7 +34,6 @@ solve1(const Blocks &blocks, Num n = 0, int z = 0, int i = 0) {
 int
 main() {
   auto blocks = Blocks{};
-  auto prog = Prog{};
   string line;
   Block b;
   while (std::getline(std::cin, line)) {
@@ -138,28 +51,9 @@ main() {
         case 'z': blocks.push_back(b); break;
       }
     }
-
-    if (inst == "inp") {
-      prog.emplace_back(INP, op1, op1);
-    } else if (inst == "add") {
-      prog.emplace_back(ADD, op1, op(op2));
-    } else if (inst == "mul") {
-      prog.emplace_back(MUL, op1, op(op2));
-    } else if (inst == "div") {
-      prog.emplace_back(DIV, op1, op(op2));
-    } else if (inst == "mod") {
-      prog.emplace_back(MOD, op1, op(op2));
-    } else if (inst == "eql") {
-      prog.emplace_back(EQL, op1, op(op2));
-    } else {
-      println("@@@ FAIL: {}", line);
-    }
-    println("{} {} {}", name(prog.back().m), prog.back().op1, to_string(prog.back().op2));
   }
 
-  println("@@@ {}", blocks);
   println("1: {}", solve1(blocks));
-  println("eval: {}", eval(prog, solve1(blocks)));
 
   return 0;
 }
