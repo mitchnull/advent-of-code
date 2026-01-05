63500c02793f7943d9c5491455d535ad79cf2bab
Péter Radics <mitchnull@gmail.com>
2026-01-05 20:08:58 +0100

    04

--- aoc-2020/src/main/cpp/04/main.cpp
+++ aoc-2020/src/main/cpp/04/main.cpp
@@ -1,33 +1,55 @@
 #include "../utils.h"
-#include <algorithm>
+#include <functional>
+#include <numeric>
 #include <sstream>
+#include <unordered_map>
 #include <unordered_set>
 
-using Num = int64_t;
-using Set = std::unordered_set<string>;
-
-static const Set Required = {
-    "byr",
-    "iyr",
-    "eyr",
-    "hgt",
-    "hcl",
-    "ecl",
-    "pid",
-};
+using Num = std::int64_t;
+using Validators = std::unordered_map<string, std::function<bool(const string &)>>;
 
 static bool
-check1(const Set &required, const string &b) {
-  Set found;
+validateRange(const string &s, Num a, Num b) {
+  int v = std::stol(s);
+  return a <= v && v <= b;
+}
+
+static const std::unordered_set<string> HairColors = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+
+static const Validators V = {
+    {"byr", [](auto s) { return validateRange(s, 1920, 2002); }},
+    {"iyr", [](auto s) { return validateRange(s, 2010, 2020); }},
+    {"eyr", [](auto s) { return validateRange(s, 2020, 2030); }},
+    {"hgt",
+        [](auto s) {
+  auto unit = s.substr(s.size() - 2);
+  if (unit == "cm") {
+    return validateRange(s.substr(0, s.size() - 2), 150, 193);
+  } else if (unit == "in") {
+    return validateRange(s.substr(0, s.size() - 2), 59, 76);
+  }
+  return false;
+}},
+    {"hcl",
+        [](auto s) {
+  return s.size() == 7 && s[0] == '#' && s.find_first_not_of("0123456789abcdef", 1) == string::npos;
+}},
+    {"ecl", [](auto s) { return HairColors.contains(s); }},
+    {"pid", [](auto s) { return s.size() == 9 && validateRange(s, 0, 999999999); }},
+};
+
+static std::pair<Num, Num>
+check(const string &b) {
+  int res1 = 0, res2 = 0;
   string entry;
   std::istringstream ss{b};
   while (ss >> entry) {
     auto k = entry.substr(0, 3);
-    if (required.contains(k)) {
-      found.insert(k);
-    }
+    auto it = V.find(k);
+    res1 += it != V.end();
+    res2 += (it != V.end() && it->second(entry.substr(4)));
   }
-  return found.size() == required.size();
+  return {res1 == V.size(), res2 == V.size()};
 }
 
 /* ------------------------------------------------------------------------ */
@@ -46,6 +68,9 @@ main() {
   }
   batches.push_back(batch);
 
-  println("1: {}", std::count_if(begin(batches), end(batches), [&](const auto &b) { return check1(Required, b); }));
+  auto [res1, res2] = std::transform_reduce(
+      begin(batches), end(batches), std::pair<Num, Num>{}, [](auto a, auto b) { return a + b; }, ::check);
+  println("1: {}", res1);
+  println("2: {}", res2);
   return 0;
 }
