ce324e16d8fe66422a74217d1121f61961dc3a86
Péter Radics <mitchnull@gmail.com>
2026-01-06 20:36:57 +0100

    11

--- aoc-2020/src/main/cpp/11/main.cpp
+++ aoc-2020/src/main/cpp/11/main.cpp
@@ -5,7 +5,7 @@ using Num = int64_t;
 using Board = Grid<>;
 
 static Num
-solve1(Board b) {
+solve(Board b, int tolerance = 4, int maxRange = 1) {
   std::vector<Pos> changed;
   do {
     changed.clear();
@@ -14,14 +14,23 @@ solve1(Board b) {
         continue;
       }
       int occ = 0;
-      for (int i = -1; i <= 1; ++i) {
-        for (int j = -1; j <= 1; ++j) {
-          occ += (i != 0 || j != 0) && b[x + j, y + i] == '#';
+      for (int dy = -1; dy <= 1; ++dy) {
+        for (int dx = -1; dx <= 1; ++dx) {
+          if (dx == 0 && dy == 0) {
+            continue;
+          }
+          for (int r = 1; r <= maxRange; ++r) {
+            auto v = b[x + dy * r, y + dx * r];
+            if (v != '.') {
+              occ += v == '#';
+              break;
+            }
+          }
         }
       }
       if (occ == 0 && b[x, y] == 'L') {
         changed.emplace_back(x, y);
-      } else if (occ >= 4 && b[x, y] == '#') {
+      } else if (occ >= tolerance && b[x, y] == '#') {
         changed.emplace_back(x, y);
       }
     }
@@ -38,7 +47,8 @@ int
 main() {
   Board b = Board::read();
 
-  println("1: {}", solve1(b));
+  println("1: {}", solve(b));
+  println("2: {}", solve(b, 5, std::numeric_limits<int>::max()));
 
   return 0;
 }
